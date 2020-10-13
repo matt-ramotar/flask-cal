50aa8a8e78ba89544a0286f7f034e0c0a99cd959
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-13 14:51:04 -0700

    Display appointments

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,34 @@
+import os
 from flask import Blueprint, render_template
+import psycopg2
 
 bp = Blueprint('main', __name__, url_prefix='')
+
+CONNECTION_PARAMETERS = {
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASS'),
+    'dbname': os.environ.get('DB_NAME'),
+    'host': os.environ.get('DB_HOST'),
+}
 
 
 @bp.route('/')
 def main():
-    return render_template('main.html')
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute('''
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+                ''')
+            rows = curs.fetchall()
+            appointments = []
+            for row in rows:
+                appointments.append({
+                    'id': row[0],
+                    'name': row[1],
+                    'start': row[2],
+                    'end': row[3]
+                })
+            print(appointments)
+    return render_template('main.html', appointments=appointments)
