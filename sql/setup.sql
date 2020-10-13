6e82bf28daf7b99a7f2a618713b33586a7b6e9ec
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-13 14:11:56 -0700

    Create db

---
+++ sql/setup.sql
@@ -0,0 +1,15 @@
+CREATE USER calendar_this WITH PASSWORD 'password' createdb;
+
+CREATE DATABASE calendar_this_dev WITH OWNER calendar_this;
+
+CREATE TABLE appointments (
+  id serial PRIMARY KEY,
+  name varchar(200) NOT NULL,
+  start_datetime timestamp NOT NULL,
+  end_datetime timestamp NOT NULL,
+  description text NOT NULL,
+  private boolean NOT NULL
+);
+
+INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+  VALUES ('Tag appointment', '2020-10-13  14:00:00', '2020-10-13 15:00:00', 'An appointment for Tag', FALSE);
